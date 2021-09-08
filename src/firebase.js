4942650d1c874473b02d3fe67187577af4cb8fd7
ahmadadejumo <adejumoahmad@gmail.com>
2021-09-08 13:20:19 +0100

    First Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAJMY7NViRj88k-nedVvv2eDis_6eW3900",
+  authDomain: "task-tracker-9cfdb.firebaseapp.com",
+  projectId: "task-tracker-9cfdb",
+  storageBucket: "task-tracker-9cfdb.appspot.com",
+  messagingSenderId: "633831839624",
+  appId: "1:633831839624:web:ef0cd3e5e24737ec4c4c3d",
+  measurementId: "G-W9WCQ78CEZ",
+};
